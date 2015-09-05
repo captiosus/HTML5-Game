5c14b2f27b60558a0ed8096f78d162e84c80c9f7
captiosus <royxu652@gmail.com>
2015-09-05 13:56:36 -0400

    gitignore

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var sys = require("sys")
-sys.puts("Hello World")
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+})
